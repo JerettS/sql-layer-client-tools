dc9f63f47ae818cc6f737e04c363631b8e819c6c
Mike McMahon <mmcm@comcast.net>
2013-08-14 12:21:54 -0400

    Scripts.

---
+++ bin/fdbsqlload.cmd
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+SETLOCAL
+
+SET CLIENT_JAR=foundationdb-sql-layer-client-tools-1.3.7-SNAPSHOT.jar
+
+IF EXIST "%~dp0..\pom.xml" GOTO FROM_BUILD
+
+REM Installation Configuration
+
+FOR %%P IN ("%~dp0..") DO SET FOUNDATIONDB_HOME=%%~fP
+
+SET CLASSPATH=%FOUNDATIONDB_HOME%\lib\%CLIENT_JAR%;%FOUNDATIONDB_HOME%\lib\client\*
+
+GOTO RUN_CMD
+
+:FROM_BUILD
+
+REM Build Configuration
+
+FOR %%P IN ("%~dp0..") DO SET BUILD_HOME=%%~fP
+
+SET CLASSPATH=%BUILD_HOME%\target\%CLIENT_JAR%;%BUILD_HOME%\target\dependency\*
+
+GOTO RUN_CMD
+
+:RUN_CMD
+java %JVM_OPTS% -cp "%CLASSPATH%" com.foundationdb.sql.client.load.LoadClient %*
+GOTO EOF
+
+:EOF
+ENDLOCAL
