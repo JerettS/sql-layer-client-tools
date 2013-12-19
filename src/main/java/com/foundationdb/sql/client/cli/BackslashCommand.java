ddb3490f0bb1e962e9521f7a5ae816dd882c7ae7
Nathan Williams <nathan.williams@foundationdb.com>
2013-12-18 19:07:16 -0500

    Outline of backslash commands and queries

---
+++ src/main/java/com/foundationdb/sql/client/cli/BackslashCommand.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2009-2013 FoundationDB, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.foundationdb.sql.client.cli;
+
+public enum BackslashCommand
+{
+    CONNECT ("c", false, false, "[SCHEMA] [USER] [HOST] [PORT]", "change connection information"),
+    CONNINFO("conninfo", false, false, "", "display connection information"),
+
+    D_TABLE     ("dt", true, true,  "[SCHEMA] NAME]",           "describe tables"),
+    D_VIEW      ("dv", true, true,  "[SCHEMA] NAME]",           "describe views"),
+
+    L_ALL       ("l",  true, false, "[[SCHEMA] NAME]",          "list tables, views and sequences"),
+    L_INDEXES   ("li", true, true,  "[[[SCHEMA] TABLE] NAME]",  "list indexes"),
+    L_SEQUENCES ("lq", true, true,  "[[SCHEMA] NAME]",          "list sequences"),
+    L_SCHEMAS   ("ls", true, true,  "[NAME]",                   "list schemata"),
+    L_TABLES    ("lt", true, true,  "[[SCHEMA] NAME]",          "list tables"),
+    L_VIEWS     ("lv", true, true,  "[[SCHEMA] NAME]",          "list views"),
+
+    HELP    ("?", false, false, "", "display this help"),
+    QUIT    ("q", false, false, "", "quit"),
+    ;
+
+    public final String cmd;
+    public final boolean hasSystem;
+    public final boolean hasDetail;
+    public final String helpCmd;
+    public final String helpArgs;
+    public final String helpDesc;
+
+
+    private BackslashCommand(String cmd, boolean hasSystem, boolean hasDetail, String helpArgs, String helpDesc) {
+        this.cmd = cmd;
+        this.helpCmd = "\\" + cmd + (hasSystem ? "[S]" : "") + (hasDetail ? "[+]" : "");
+        this.hasSystem = hasSystem;
+        this.hasDetail = hasDetail;
+        this.helpArgs = helpArgs;
+        this.helpDesc = helpDesc;
+    }
+}
