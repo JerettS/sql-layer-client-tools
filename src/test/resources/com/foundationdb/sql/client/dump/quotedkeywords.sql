ff4a74837e8ecf9934fa72df04af266a57619b92
SytzeH <sytze.harkema@foundationdb.com>
2014-03-17 13:15:24 -0400

    added test case for automated quoted (protected) keywords as identifiers

---
+++ src/test/resources/com/foundationdb/sql/client/dump/quotedkeywords.sql
@@ -0,0 +1,37 @@
+
+--- as
+
+DROP TABLE IF EXISTS `as`;
+
+CREATE TABLE `as`(
+  testid INT
+);
+
+
+
+INSERT INTO `as` VALUES(3);
+
+--- max
+
+DROP TABLE IF EXISTS `max`;
+
+CREATE TABLE `max`(
+  testid INT
+);
+
+
+
+INSERT INTO `max` VALUES(2);
+
+--- values
+
+DROP TABLE IF EXISTS `values`;
+
+CREATE TABLE `values`(
+  testid INT
+);
+
+
+
+INSERT INTO `values` VALUES(1);
+
